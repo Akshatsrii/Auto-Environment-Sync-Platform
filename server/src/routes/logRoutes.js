62f09621c1db213941764d48fc8984bc4bd5666d
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-06-20 11:34:28 +0530

    Set up audit log collection and persistence layer

---
+++ server/src/routes/logRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { getLogs, getLogById } = require('../controllers/logController')
+const { protect } = require('../middleware/authMiddleware')
+
+router.use(protect)
+
+router.get('/', getLogs)
+router.get('/:id', getLogById)
+
+module.exports = router
