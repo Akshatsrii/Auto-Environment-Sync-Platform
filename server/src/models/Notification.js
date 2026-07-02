5d42413c939d7605e345516cb7e704a5470a25c1
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-07-02 12:49:34 +0530

    Create notification schema for persistent user notifications

---
+++ server/src/models/Notification.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const notificationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['drift', 'sync', 'error', 'info'],
+    default: 'info',
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  meta: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {},
+  },
+}, { timestamps: true })
+
+module.exports = mongoose.model('Notification', notificationSchema)
